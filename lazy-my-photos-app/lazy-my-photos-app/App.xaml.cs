c93a95bf54a358c8dfdd9ca78cd8304a2136806b
Serghei Adam <adam.serghei@gmail.com>
2024-08-15 01:02:29 +0300

    Project init

---
+++ lazy-my-photos-app/lazy-my-photos-app/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace lazy_my_photos_app;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ lazy-my-photos-app/lazy-my-photos-app/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace lazy_my_photos_app
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
