6f23c2efd169dbc9bea2d482d55adf643cda11bc
Serghei Adam <adam.serghei@gmail.com>
2024-08-16 00:55:44 +0300

    Added command to login/register buttons

--- lazy-my-photos-app/lazy-my-photos-app/ViewModel/User/LoginViewModel.cs
+++ lazy-my-photos-app/lazy-my-photos-app/ViewModel/User/LoginViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace lazy_my_photos_app.ViewModel.User
 {
-    internal class LoginViewModel
+    [ObservableObject]
+    public partial class LoginViewModel
     {
+        [RelayCommand]
+        async Task Register()
+        {
+            await Shell.Current.GoToAsync("RegisterPage");
+        }
+
     }
 }
--- lazy-my-photos-app/lazy-my-photos-app/ViewModel/User/RegisterViewModel.cs
+++ lazy-my-photos-app/lazy-my-photos-app/ViewModel/User/RegisterViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace lazy_my_photos_app.ViewModel.User
 {
-    internal class RegisterViewModel
+
+    [ObservableObject]
+    public partial class RegisterViewModel
     {
+        [RelayCommand]
+        async Task Login()
+        {
+            await Shell.Current.GoToAsync("LoginPage");
+        }
     }
 }
